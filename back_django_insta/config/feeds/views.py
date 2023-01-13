f2bb0c79e8677080b81af498dc51ce506f17ad97
jaeupsu <hyeongs2323@gmail.com>
2023-01-13 17:04:54 +0900

    refix: db debuging

--- back_django_insta/config/feeds/views.py
+++ back_django_insta/config/feeds/views.py
@@ -32,7 +32,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from rest_framework.decorators import api_view
 
 from .models import Feed
 from .serializers import FeedsSerializer, FeedDetailSerializer
