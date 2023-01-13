b6895ea52c9f5a113ea4c64728447cee51f131eb
jaeupsu <hyeongs2323@gmail.com>
2023-01-13 17:06:26 +0900

    feat: users urls add

--- back_django_insta/config/users/urls.py
+++ back_django_insta/config/users/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     # path("", views.show_user),
-    path("myinfo", views.MyInfo.as_view())
+    path("myinfo", views.MyInfo.as_view()),
+    path("", views.Users.as_view()),
+    path("login", views.Login.as_view()),
+    path("logout", views.Logout.as_view()),
 ]
